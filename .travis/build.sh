ef05a775bcc83bf6fd13c410a0f6d9312b8415da
Bryan Davis <bd808@bd808.com>
2014-11-09 23:00:45 -0700

    Show the config files being read

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
+[[ -f /etc/hhvm/php.ini ]] && cat /etc/hhvm/php.ini
+[[ -f /etc/hhvm/config.hdf ]] && cat /etc/hhvm/config.hdf
+
 hhvm ${HHVM_FLAGS} --php vendor/phpunit/phpunit/phpunit
