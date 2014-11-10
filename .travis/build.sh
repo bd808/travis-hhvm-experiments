9ad1cd9d57b3adf7a3d4e49a43fcd8853efb58dd
Bryan Davis <bd808@bd808.com>
2014-11-09 23:31:46 -0700

    Check for session storage dir

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,4 +4,10 @@ set -ex
 [[ -f /etc/hhvm/php.ini ]] && cat /etc/hhvm/php.ini
 [[ -f /etc/hhvm/config.hdf ]] && cat /etc/hhvm/config.hdf
 
+if [[ -d /var/lib/php5 ]]; then
+  ls -ld /var/lib/php5
+else
+  echo Default session storage directory not found.
+fi
+
 hhvm ${HHVM_FLAGS} --php vendor/phpunit/phpunit/phpunit
